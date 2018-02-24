6006e15aeeeaa869be540f91ebffc92f33b73bf9
Sobrino Julian <sobrinojulian@protonmail.com>
2018-02-24 10:22:49 -0300

    Prettier

--- cli.js
+++ cli.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
-'use strict';
-const meow = require('meow');
-const Conf = require('conf');
-const moment = require('moment');
-const mortem = require('./mortem');
+"use strict";
+const meow = require("meow");
+const Conf = require("conf");
+const moment = require("moment");
+const mortem = require("./mortem");
 
-const isValidDate = (date) => moment(date, 'YYYY/MM/DD', true).isValid();
+const isValidDate = date => moment(date, "YYYY/MM/DD", true).isValid();
 
-const isDateSet = (config) => config.has('date');
+const isDateSet = config => config.has("date");
 
-const errorAndExit = (message) => {
+const errorAndExit = message => {
   console.error(message);
   const failure = 1;
   process.exit(failure);
-}
+};
 
 const sbd = (date, config) => {
-  if (!isValidDate(date)) return false;  
-  config.set('date', date);
+  if (!isValidDate(date)) return false;
+  config.set("date", date);
   return true;
-}
+};
 
 const cli = meow(`
     Usage
@@ -49,26 +49,26 @@ const cli = meow(`
 
 const config = new Conf();
 const command = cli.input[0];
-if (!isDateSet(config) && command !== 'sbd') errorAndExit('Date is unset');
-const d = new Date(config.get('date')); 
+if (!isDateSet(config) && command !== "sbd") errorAndExit("Date is unset");
+const d = new Date(config.get("date"));
 switch (command) {
-  case 'sbd':
+  case "sbd":
     const input = cli.input[1];
-    if (!sbd(input, config)) errorAndExit('Invalid date');
+    if (!sbd(input, config)) errorAndExit("Invalid date");
     break;
-  case 'gbd':
-    console.log(moment(d).format('YYYY/MM/DD'));
+  case "gbd":
+    console.log(moment(d).format("YYYY/MM/DD"));
     break;
-  case 'ndl':
+  case "ndl":
     console.log(mortem.ndl(d));
     break;
-  case 'pro':
-    console.log(mortem.pro(d).toFixed(2) + '%');
+  case "pro":
+    console.log(mortem.pro(d).toFixed(2) + "%");
     break;
-  case 'etr':
+  case "etr":
     console.log(mortem.etr(d));
     break;
-  case 'eyd':
+  case "eyd":
     console.log(mortem.eyd(d));
     break;
   default:
